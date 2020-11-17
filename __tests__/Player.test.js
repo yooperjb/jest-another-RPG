6655f851e3936fb687ed8a74e58f6b12b8954540
Pedal <jmb181@humboldt.edu>
2020-11-17 00:10:33 -0800

    Constructor methods

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,6 +1,3 @@
-// const { jsxText } = require('@babel/types');
-// const { test, expect, jest } = require('@jest/globals');
-const { expect } = require('@jest/globals');
 const Player = require('../lib/Player');
 const Potion = require('../lib/Potion');
 
@@ -18,4 +15,23 @@ test('creates a player object', () => {
     expect(player.inventory).toEqual(
         expect.arrayContaining([expect.any(Object)])
     );
+});
+
+test("gets player's stats as an object", () => {
+    const player = new Player('Dave');
+  
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+  });
+
+test('gets inventory from player or returns false', () => {
+const player = new Player('Dave');
+
+expect(player.getInventory()).toEqual(expect.any(Array));
+
+player.inventory = [];
+
+expect(player.getInventory()).toEqual(false);
 });
